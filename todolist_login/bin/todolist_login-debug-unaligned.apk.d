84c1e34722d63f56d1804f384bb8f9e41ce1559e
Violet-GitHub <c20121101@sina.com>
2015-09-29 17:56:00 +0800

    ant clean debug

---
+++ todolist_login/bin/todolist_login-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+E:\study\GitHub\CI\todolist_login\bin\todolist_login-debug-unaligned.apk : \
+E:\study\GitHub\CI\todolist_login\bin\todolist_login.ap_ \
+E:\study\GitHub\CI\todolist_login\bin\classes.dex \
+E:\study\GitHub\CI\todolist_login\src\com\robotium\solo\RobotiumElement.js \
+E:\study\GitHub\CI\todolist_login\libs\android-junit-report-1.5.8.jar \
---
+++ todolist_login/bin/todolist_login.ap_.d
@@ -0,0 +1,11 @@
+E:\study\GitHub\CI\todolist_login\bin\todolist_login.ap_ \
+ : E:\study\GitHub\CI\todolist_login\res\drawable-hdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\res\drawable-ldpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\res\drawable-mdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\res\drawable-xhdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\res\values\strings.xml \
+E:\study\GitHub\CI\todolist_login\bin\res\drawable-hdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\bin\res\drawable-ldpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\bin\res\drawable-mdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\bin\res\drawable-xhdpi\ic_launcher.png \
+E:\study\GitHub\CI\todolist_login\bin\AndroidManifest.xml \
