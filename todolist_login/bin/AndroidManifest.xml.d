9648caa66c5f1428c5ffe05374fa8f84dcd19e25
Violet-GitHub <c20121101@sina.com>
2015-10-08 16:16:19 +0800

    delete all

--- todolist_login/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\study\GitHub\CI\todolist_login\bin\AndroidManifest.xml : \
-E:\study\GitHub\CI\todolist_login\AndroidManifest.xml \
--- todolist_login/bin/todolistTest-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-E:\study\GitHub\CI\todolist_login\bin\todolistTest-debug-unaligned.apk : \
-E:\study\GitHub\CI\todolist_login\bin\todolistTest.ap_ \
-E:\study\GitHub\CI\todolist_login\bin\classes.dex \
-E:\study\GitHub\CI\todolist_login\src\com\robotium\solo\RobotiumElement.js \
-E:\study\GitHub\CI\todolist_login\libs\android-junit-report-1.5.8.jar \
