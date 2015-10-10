193ae153efdbca12deebc81b64586b1062bfad42
Violet-GitHub <c20121101@sina.com>
2015-10-10 12:25:13 +0800

    run.sh change

--- todolist_login/run.sh
+++ todolist_login/run.sh
@@ -9,4 +9,7 @@ echo "start to install test apk"
 adb install -r todolist_login/bin/todolistTest-debug.apk
 echo "start to run test"
 adb shell am instrument -w -e reportDir /mnt/sdcard/testauto/junit/ -e reportFile junit-reportFile.xml com.example.todolist.test/com.example.todolist.test.runner.Runner1
+echo "pull junit report"
 adb pull /mnt/sdcard/testauto/junit/junit-reportFile.xml $WORKSPACE/junit-reportFile.xml
+echo "pull screenshots"
+adb pull /sdcard/Robotium-Screenshots/ $WORKSPACE/
