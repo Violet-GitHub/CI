58c619ef1f7a5d41f4d1ee467b8fe1b9fcdbe764
Violet-GitHub <c20121101@sina.com>
2015-10-10 14:09:01 +0800

    False

--- todolist_login/src/com/example/todolist/test/testcase/login/Login3.java
+++ todolist_login/src/com/example/todolist/test/testcase/login/Login3.java
@@ -7,6 +7,6 @@ public class Login3 extends BasicTestCase{
 	public void testLogin1(){
 		uiHelper.getElementsLoginActivity().login(new String[]{"1","1"});
 		solo.sleep(1000);
-		assertTrue(uiHelper.getElementsMainActivity().getActionNew().isShown());
+		assertFalse(uiHelper.getElementsMainActivity().getActionNew().isShown());
 	}
 }
