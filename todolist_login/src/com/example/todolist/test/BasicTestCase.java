7e26c42e4a06dc07f4baa98e9a0c2e50eebfd961
Violet-GitHub <c20121101@sina.com>
2015-10-10 11:49:25 +0800

    takeScreen

--- todolist_login/src/com/example/todolist/test/BasicTestCase.java
+++ todolist_login/src/com/example/todolist/test/BasicTestCase.java
@@ -19,8 +19,25 @@ public class BasicTestCase extends ActivityInstrumentationTestCase2<LoginActivit
 	
 	@Override
 	protected void setUp() throws Exception {
-		super.setUp();
-		init();
+		try {
+			super.setUp();
+			init();
+		} catch (Throwable tr) {
+			solo.takeScreenshot(this.getClass().getSimpleName());
+			throw new SetUpException(tr);
+		}
+		
+	}
+	
+	@Override
+	protected void runTest() throws Throwable {
+		try {
+			super.runTest();
+		} catch (Throwable tr) {
+			solo.takeScreenshot(this.getClass().getSimpleName());
+			throw new RunTestException(tr);
+		}
+		
 	}
 	
 	public void init(){
@@ -39,8 +56,57 @@ public class BasicTestCase extends ActivityInstrumentationTestCase2<LoginActivit
 
 	@Override
 	protected void tearDown() throws Exception {
-		solo.finishOpenedActivities();
-		super.tearDown();
+		try {
+			if (wakelockobject != null ) {
+				wakelockobject.release();
+				wakelockobject=null;
+			}
+			solo.finishOpenedActivities();
+			uiHelper=null;
+			super.tearDown();
+		} catch (Throwable tr) {
+			solo.takeScreenshot(this.getClass().getSimpleName());
+			throw new TearDownException(tr);
+		}
+	}
+	
+	
+	@Override
+	public void runBare() throws Throwable {
+		try {
+			super.runBare();
+		} catch (SetUpException sue) {
+			tearDown();
+			throw sue;
+		}catch (RunTestException rte) {
+			tearDown();
+			throw rte;
+		}catch (TearDownException tde) {
+			tearDown();
+			throw tde;
+		}
+	}
+	
+
+	class SetUpException extends Exception{
+		public static final long serialVersionUID = 1L;
+		public SetUpException(Throwable e) {
+			super("Errer in BasicTestCase.setUp()! ",e);
+		}
+	}
+
+	class RunTestException extends Exception{
+		public static final long serialVersionUID = 2L;
+		public RunTestException(Throwable e) {
+			super("Errer in BasicTestCase.runTest()!",e);
+		}
+	}
+	class TearDownException extends Exception{
+		public static final long serialVersionUID = 3L;
+		public TearDownException(Throwable e) {
+			super("Errer in BasicTestCase.tearDown()!", e);
+		}
 	}
 	
 }
+
