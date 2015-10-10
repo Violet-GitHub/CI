76daef91ab5be780a6993fa92575722fbb7d8c40
Violet-GitHub <c20121101@sina.com>
2015-10-10 14:51:30 +0800

    run.sh

--- todolist_login/run.sh
+++ todolist_login/run.sh
@@ -5,12 +5,12 @@ cd todolist_login
 ant clean debug
 cd ..
 echo "start to install source apk"
-adb install -r todolist/bin/todolist-debug.apk
+adb -s $1 install -r todolist/bin/todolist-debug.apk
 echo "start to install test apk"
-adb install -r todolist_login/bin/todolistTest-debug.apk
+adb -s $1 install -r todolist_login/bin/todolistTest-debug.apk
 echo "start to run test"
-adb shell am instrument -w -e reportDir /mnt/sdcard/testauto/junit/ -e reportFile junit-reportFile.xml com.example.todolist.test/com.example.todolist.test.runner.Runner1
+adb -s $1 shell am instrument -w -e reportDir /mnt/sdcard/testauto/junit/ -e reportFile junit-reportFile.xml com.example.todolist.test/com.example.todolist.test.runner.Runner1
 echo "pull junit report"
-adb pull /mnt/sdcard/testauto/junit/junit-reportFile.xml $WORKSPACE/junit-reportFile.xml
+adb -s $1 pull /mnt/sdcard/testauto/junit/junit-reportFile.xml $WORKSPACE/junit-reportFile.xml
 echo "pull screenshots"
-adb pull /sdcard/Robotium-Screenshots/ $WORKSPACE/
+adb -s $1 pull /sdcard/Robotium-Screenshots/ $WORKSPACE/
